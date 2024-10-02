5a47e987fc9f6577674c20998dfab7ff68c752ac
E-moL0587 <kai.pika.025@outlook.com>
2024-10-02 20:21:52 +0900

    update

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+    origin: 'https://next-nest-app.onrender.com',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
   });
   await app.listen(3001, '0.0.0.0');
 }
